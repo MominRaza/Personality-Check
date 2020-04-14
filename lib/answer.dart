f4098c32639d109e066e6475ae9d40cb1dcf9163
MominRaza <mominr082@gmail.com>
2020-04-14 18:30:08 +0530

    Personality Check App First Commit

---
+++ lib/answer.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 5,
+      ),
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(
+          answerText,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
